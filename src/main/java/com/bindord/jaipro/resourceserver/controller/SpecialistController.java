acf3e4115943b812de51c8dbc23b6731cf3677b1
Panda <63072537+giovanny110@users.noreply.github.com>
2023-02-19 18:44:01 -0500

    HEP-131: Specialist create and upload file 50%

--- src/main/java/com/bindord/jaipro/resourceserver/controller/SpecialistController.java
+++ src/main/java/com/bindord/jaipro/resourceserver/controller/SpecialistController.java
@@ -3,8 +3,11 @@ package com.bindord.jaipro.resourceserver.controller;
 import com.bindord.jaipro.resourceserver.advice.CustomValidationException;
 import com.bindord.jaipro.resourceserver.advice.NotFoundValidationException;
 import com.bindord.jaipro.resourceserver.domain.specialist.Specialist;
+import com.bindord.jaipro.resourceserver.domain.specialist.dto.SpecialistCvDto;
 import com.bindord.jaipro.resourceserver.domain.specialist.dto.SpecialistDto;
 import com.bindord.jaipro.resourceserver.domain.specialist.dto.SpecialistUpdateDto;
+import com.bindord.jaipro.resourceserver.service.googleCloud.googleCloudService;
+import com.bindord.jaipro.resourceserver.service.specialist.SpecialistCvService;
 import com.bindord.jaipro.resourceserver.service.specialist.SpecialistService;
 import com.bindord.jaipro.resourceserver.validator.Validator;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -20,10 +23,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.io.File;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -35,6 +40,8 @@ public class SpecialistController {
     private final Validator validator;
 
     private final SpecialistService specialistService;
+    private final SpecialistCvService specialistCvService;
+    private final googleCloudService googleCloudService;
 
     @ApiResponse(description = "Persist a specialist",
             responseCode = "200")
@@ -43,7 +50,17 @@ public class SpecialistController {
             consumes = {MediaType.APPLICATION_JSON_VALUE})
     public Mono<Specialist> save(@Valid @RequestBody SpecialistDto specialist)
             throws NotFoundValidationException, CustomValidationException {
-        return specialistService.save(specialist);
+        Mono<Specialist> rSpecialist =  specialistService.save(specialist);
+
+        /*SpecialistCvDto specialistCvDto = new SpecialistCvDto();
+        specialistCvDto.setAbout(specialist.getAbout());
+        specialistCvDto.setProfilePhoto(specialist.getProfilePhoto());
+        rSpecialist.then((e) ->{
+            specialistCvDto.setId(e.getId());
+        });*/
+
+        //specialistCvService.save(specialistCvDto);
+        return rSpecialist;
     }
 
     @ApiResponse(description = "Update a specialist",
@@ -79,5 +96,15 @@ public class SpecialistController {
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public Mono<Boolean> existsSpecialistByDocument(@RequestParam String document) throws NotFoundValidationException {
         return specialistService.existsSpecialistByDocument(document);
+    }
+
+    @PostMapping(value = "/files")
+    public Mono<String> test(@RequestParam("file") File file){
+        try{
+            googleCloudService.saveFile(file, "test");
+            return Mono.just("TEst");
+        }catch (Exception ex){
+            return Mono.just(ex.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/bindord/jaipro/resourceserver/service/googleCloud/googleCloudService.java
@@ -0,0 +1,9 @@
+package com.bindord.jaipro.resourceserver.service.googleCloud;
+
+import java.io.File;
+
+public interface googleCloudService {
+
+    boolean saveFile(File file, String ruta);
+
+}
