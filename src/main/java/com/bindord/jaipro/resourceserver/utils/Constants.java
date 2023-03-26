37d35072c3ed34b0b567b3c6de6b1403aa3f0dc0
Petter Carranza <peterpaul.0194@gmail.com>
2023-03-26 18:50:14 -0500

    fix of dependency injections error

--- src/main/java/com/bindord/jaipro/resourceserver/utils/Constants.java
+++ src/main/java/com/bindord/jaipro/resourceserver/utils/Constants.java
@@ -3,4 +3,5 @@ package com.bindord.jaipro.resourceserver.utils;
 public class Constants {
 
     public static final String RESOURCE_NOT_FOUND = "Resource not found";
+    public static final Integer MAX_GALLERY_FILES = 6;
 }
